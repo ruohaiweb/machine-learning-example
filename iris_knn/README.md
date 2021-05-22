1674ddd66f0f1921bd2c68285d5fc054882875e2
yajun <yajun@example.com>
2021-05-22 23:32:41 +0800

    210522, iris_knn, sklearn

---
+++ iris_knn/README.md
@@ -0,0 +1,7 @@
+同名公众号和B站：极客若海
+
+公众号讲义链接：
+
+https://mp.weixin.qq.com/s/az5QI__MCxBiuWobKCfbLA
+
+B站视频讲解链接：
---
+++ iris_knn/iris_knn.py
@@ -0,0 +1,28 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
+
+# 1.获取数据集
+iris = load_iris()
+
+# 2.数据基本处理
+# x_train,x_test,y_train,y_test为训练集特征值、测试集特征值、训练集目标值、测试集目标值
+x_train, x_test, y_train, y_test = train_test_split(iris.data, iris.target, test_size=0.2, random_state=22)
+
+# 3、特征工程：标准化
+transfer = StandardScaler()
+x_train = transfer.fit_transform(x_train)
+x_test = transfer.transform(x_test)
+
+# 4、机器学习(模型训练)
+estimator = KNeighborsClassifier(n_neighbors=9)
+estimator.fit(x_train, y_train)
+# 5、模型评估
+# 方法1：比对真实值和预测值
+y_predict = estimator.predict(x_test)
+print("预测结果为:\n", y_predict)
+print("比对真实值和预测值：\n", y_predict == y_test)
+# 方法2：直接计算准确率
+score = estimator.score(x_test, y_test)
+print("准确率为：\n", score)
